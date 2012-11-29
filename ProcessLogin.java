e284e4e6d6e61ed8e395f791d1e003da3877ddb1
James Thomas Dressel <jtdressel@ONI.(none)>
2012-11-28 23:59:29 -0500

    Add processsing to ProcessLogin
    Now it takes posted fields and does magic.

--- ProcessLogin.java
+++ ProcessLogin.java
@@ -3,20 +3,29 @@ package jdressel.Derporia64;
 import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
- 
+import java.io.*;
+
+
 public class ProcessLogin extends HttpServlet {
- 
+
+private String username;
 public void doGet(HttpServletRequest request, HttpServletResponse res)
 throws ServletException, IOException  {
  
-/*
-* Send the user back to where they belong
-*/
+	/*
+	* Send the user back to where they belong
+	*/
  
-String destination="http://apps-swe432.vse.gmu.edu:8080/swe432/jsp/jdressel/Derporia64/Login.jsp";        
-res.sendRedirect(response.encodeRedirectURL(destination));
+	String destination="http://apps-swe432.vse.gmu.edu:8080/swe432/jsp/jdressel/Derporia64/Login.jsp";        
+	res.sendRedirect(res.encodeRedirectURL(destination));
  
  }
-
+public void doPost(HttpServletRequest request, HttpServletResponse res) throws ServletException, IOException {
+	username = request.getParameter("username")==null ? "" : request.getParameter("username");
+	res.setContentType ("text/html");
+	PrintWriter out = res.getWriter();
+	out.println(username);
+        out.close ();
+}
 
 }
