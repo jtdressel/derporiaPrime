81c39536c31908d7decc48f4ba8fb546e19c35b7
James Thomas Dressel <jtdressel@ONI.(none)>
2012-11-28 23:50:08 -0500

    Refactor ProcessLogin.java
    Make it look more like the other files.

--- ProcessLogin.java
+++ ProcessLogin.java
@@ -1,22 +1,22 @@
 package jdressel.Derporia64;
+//James Dressel and James Robertson
 import java.io.IOException;
- 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
  
 public class ProcessLogin extends HttpServlet {
  
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
+public void doGet(HttpServletRequest request, HttpServletResponse res)
 throws ServletException, IOException  {
  
 /*
 * Send the user back to where they belong
 */
  
-String destination  ="http://apps-swe432.vse.gmu.edu:8080/swe432/jsp/jdressel/Derporia64/Login.jsp";        
-response.sendRedirect(response.encodeRedirectURL(destination));
+String destination="http://apps-swe432.vse.gmu.edu:8080/swe432/jsp/jdressel/Derporia64/Login.jsp";        
+res.sendRedirect(response.encodeRedirectURL(destination));
  
  }
+
+
 }
