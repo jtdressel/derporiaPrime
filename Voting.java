49473dc0d53e2b84e527428710a612178f15e582
James Thomas Dressel <jtdressel@ONI.(none)>
2012-11-29 11:29:14 -0500

    Check for nulls

--- Voting.java
+++ Voting.java
@@ -19,10 +19,22 @@ public void doGet(HttpServletRequest request, HttpServletResponse res) throws Se
 	PrintWriter out = res.getWriter ();
 	HttpSession session = request.getSession();
 	
-	out.println(getServletContext().getAttribute("jdresselAssertionSet"));
+	
 	Object d = getServletContext().getAttribute("jdresselAssertionSet");
-		Set<Assertion> asse = (Set<Assertion>)d;
-	out.println(asse.size());
+	
+	if(d==null){
+
+	} else {
+Set<Assertion> assertions = (Set<Assertion>)d;
+
+	for(Iterator<Assertion> assertionIterator = assertions.iterator(); assertionIterator.hasNext();){
+		out.println(assertionIterator.next());
+		out.println("<br>");
+	}
+	}
+
+	
+
 	out.println("<a href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/jsp/jdressel/Derporia64/Derporia.jsp\"> home</a>");
         out.close();
     }
