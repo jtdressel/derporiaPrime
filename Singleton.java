a32d692794282c231a5da6f3e8c071fce3c889af
James Thomas Dressel <jtdressel@ONI.(none)>
2012-11-29 04:33:40 -0500

    Give singleton bits to reset things

--- Singleton.java
+++ Singleton.java
@@ -1,6 +1,7 @@
 package jdressel.Derporia64;
 import java.util.*;
 
+
 /*	
  * 	This class contains assertions made by users
  * 	@author James Dressel
@@ -9,7 +10,7 @@ public class Singleton {
 	
 	private static Set<Assertion> assertions = new HashSet<Assertion>();
 	private static Set<User> users = new HashSet<User>();
-
+	private static Set<HttpSession> sessions = new HashSet<HttpSession>();
 
 	private Singleton(){
 		throw new AssertionError();//From the bloch book. Keep from being constructed
@@ -31,12 +32,36 @@ public class Singleton {
 		users.add(user);
 	}
 
+	public static void addSession(HttpSession session){
+		users.add(session);
+	}
+
 	public static void removeAssertion(Assertion assertion){
 		assertions.remove(assertion);
 	}
 
 	public static void removeUser(User user){
 		users.remove(user);
+	}
+
+	public static void clearAssertions(){
+		assertions.clear();
+	}
+
+	public static void clearUsers(){
+		users.clear();
+	}
+
+	public static void resetSessions(){
+		for (HttpSession session: sessions){
+			session.invalidate();//TODO: make sure all sessions are in this set
+		}	
+	}
+
+	public static void reset(){
+		clearAssertions();
+		clearUsers();
+		resetSessions();
 	}
 	
 }
