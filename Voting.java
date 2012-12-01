137dc4bf4e9e9579d26dc6db56c777c1d3f3caae
James Thomas Dressel <jtdressel@ONI.(none)>
2012-12-01 14:58:21 -0500

    Remvoe buttons.

--- Voting.java
+++ Voting.java
@@ -104,22 +104,17 @@ public void doGet(HttpServletRequest request, HttpServletResponse res) throws Se
 				out.println("												<table width=\"100%\">");
 				out.println("													<tbody><tr>");
 				out.println("													<td align=\"left\">");
-				out.println("<a class=\".red\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=disagree&id=" + assertion.getId() +" \">disagree</a>");//TODO make button work
-				out.println("<form method=\"link\" action=\"\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=disagree&id=" + assertion.getId() +" \"">");
-				out.println("														<button type=\"submit\" onclick=\"submit();\" class=\"red\" name=\"disagree\">Disagree</button> ");
-				out.println("</form>");
+				out.println("<a class=\".red\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=disagree&id=" + assertion.getId() +" \">Disagree</a>");
+				
+																
 				out.println("													</td>");
 				out.println("													<td align=\"center\">");
-				out.println("<a class=\".yellow\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=unsure&id=" + assertion.getId() +" \">unsure</a>");//TODO make button work
-				out.println("<form method=\"link\" action=\"\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=unsure&id=" + assertion.getId() +" \"">");
-				out.println("														<button type=\"submit\" onclick=\"submit();\" class=\"yellow\" name=\"unsure\">Unsure</button> ");
-				out.println("</form>");
+				out.println("<a class=\".yellow\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=unsure&id=" + assertion.getId() +" \">Unsure</a>");
+				
 				out.println("													</td>");
 				out.println("													<td align=\"right\">");
-				out.println("<a class=\".green\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=convinced&id=" + assertion.getId() +" \">Convinced</a>");//TODO make button work
-				out.println("<form method=\"link\" action=\"\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=convinced&id=" + assertion.getId() +" \"">");
-				out.println("														<button type=\"submit\" onclick=\"submit();\" class=\"green\" name=\"convinced\">Convinced</button> ");
-				out.println("</form>");
+				out.println("<a class=\".green\" href=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessVote?vote=convinced&id=" + assertion.getId() +" \">Convinced</a>");
+				
 				out.println("													</td>");
 				out.println("													</tr>");
 				out.println("												</tbody></table>");
