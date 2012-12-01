ee724b8919740db433c25ce43d7a148df95ba135
James Robertson <ubergeek@jamesrobertson.org>
2012-11-30 21:00:32 -0500

    fix extra dash

--- Utility.java
+++ Utility.java
@@ -13,7 +13,7 @@ public class Utility {
 			username = "";
 		}
 		if (username==""||username==null){
-			return("<div class=\"username\">\n<form name=\"loginForm\"  action=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessLogin\" method=\"post\">\nLog In: <input type=\"text\" name=\"username\" placeholder=\"Username\" onkeypress=\"checkEnter(event)\">\n	<input class=\"regular\" type=\"submit\" name=\"Log In\"/>\n</form>\\</div>");
+			return("<div class=\"username\">\n<form name=\"loginForm\"  action=\"http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.ProcessLogin\" method=\"post\">\nLog In: <input type=\"text\" name=\"username\" placeholder=\"Username\" onkeypress=\"checkEnter(event)\">\n	<input class=\"regular\" type=\"submit\" name=\"Log In\"/>\n</form></div>");
 		} 
 
 		return("<div class=\"username\">You are logged in as "+username+" <a href=http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.Logout> Logout</a></div>");
