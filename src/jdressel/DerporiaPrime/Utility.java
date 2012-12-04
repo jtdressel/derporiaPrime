4398912abac03f3d38540ccb25ebe4d3cedcf58a
James Dressel <ubergeek@jamesdressel.com>
2012-12-04 13:17:11 -0500

    Create getusername methods in Utility

--- src/jdressel/DerporiaPrime/Utility.java
+++ src/jdressel/DerporiaPrime/Utility.java
@@ -1,5 +1,7 @@
 package jdressel.DerporiaPrime;
 
+import javax.servlet.http.HttpSession;
+
 public class Utility {
 
 	public static String loginHeaderBanner(Object userObject){
@@ -17,6 +19,34 @@ public class Utility {
 		} 
 
 		return("<div class=\"username\">You are logged in as "+username+" <a href=http://apps-swe432.vse.gmu.edu:8080/swe432/servlet/jdressel.Derporia64.Logout> Logout</a></div>");
+	}
+	
+	public static String getUsername(Object userObject){
+		String username = "";
+		//Determine user
+		try{
+			User user = (User) userObject;
+			username = user.getUN();
+		} catch (ClassCastException e){
+			//Assume logged out if exception occurs
+			username = "";
+		}
+		return username;
+	}
+	
+	public static String getUsername(HttpSession session){
+		//Object derp  = request.getSession().getAttribute("username")==null ? "" : request.getSession().getAttribute("username");
+		//out.println(Utility.loginHeaderBanner(derp));
+		Object userAttribute = session.getAttribute("username");
+		String username = "";
+		try{
+			User user = (User) userAttribute;
+			username = user.getUN();
+		} catch (ClassCastException e){
+			username = "";//TODO raise exception if user does not exist
+		}
+
+		return username;
 	}
 
 
