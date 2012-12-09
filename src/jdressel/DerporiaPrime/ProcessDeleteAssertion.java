ec986aa0a4ab9f4553702f9600bc6516e056f589
James Dressel <ubergeek@jamesdressel.com>
2012-12-09 18:20:05 -0500

    Let users delete their assertions

---
+++ src/jdressel/DerporiaPrime/ProcessDeleteAssertion.java
@@ -0,0 +1,70 @@
+package jdressel.DerporiaPrime;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProcessDeleteAssertion
+ */
+public class ProcessDeleteAssertion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessDeleteAssertion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String id = request.getParameter("id")==null ? "" : request.getParameter("id");
+		Object d = getServletContext().getAttribute("jdresselAssertionSet");
+		//TODO Checks and stuff to make sure this runs
+		try{
+			Set assertions = (Set) d;
+			Assertion underVote = null;
+
+    		for(Iterator<Assertion> assertionIterator = assertions.iterator(); assertionIterator.hasNext();){
+    			Assertion assertion = assertionIterator.next();
+    			
+    			if(assertion.getId().equals(id)){
+    				
+    				underVote = assertion;
+    				assertions.remove(underVote);
+    				getServletContext().setAttribute("jdresselAssertionSet", assertions);
+
+
+    				response.sendRedirect(response.encodeRedirectURL("Voting.jsp"));
+    			}
+    		
+    		}
+
+    		
+    	
+		}
+		catch (Exception e){
+			response.sendRedirect(response.encodeRedirectURL("Voting.jsp"));
+			//TODO not this
+		}
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect(response.encodeRedirectURL("Voting.jsp"));
+	}
+}
